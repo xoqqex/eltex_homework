ab11a1b1959a2ca0176bb1cff17dcbc38e87533c
xoqqex <xoqqex@gmail.com>
2024-11-27 21:09:18 +0700

    editing

---
+++ module_2/task1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#define NUM 2
+
+int main (){
+	int n;
+	
+	printf("Введите натуральное число N: ");
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++){
+	*p *= NUM;
+	}	
+
+	printf("result = %d\n", result);
+
+	return 0;
+}
